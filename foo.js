24f006f42dc35e40861b5409eea630594ee8cad0
Victor Cruceanu <39226366+PsyMadDoc@users.noreply.github.com>
2019-10-25 11:18:00 +0300

    added fullpage.js, adjusted elements sizing

---
+++ foo.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  loopBottom: true,
+  fixedElements: '#nav',
+  navigation: true,
+  navigationTooltips: ['Welcome!', 'Get to know me!', 'Footer']
+});
